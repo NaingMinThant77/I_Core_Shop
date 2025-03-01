d4b27c6d8ac40824a78c049a6ebd4f9119e3dbce
NaingMinThant77 <naingminthant07@gmail.com>
2025-03-01 15:37:45 +0630

    Session-extend

--- components/settings/two-factor.tsx
+++ components/settings/two-factor.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import SettingCard from './setting-card'
 import { Button } from '../ui/button'
 import { Check, X } from 'lucide-react'
+import { auth } from '@/server/auth'
 
-const TwoFactor = () => {
+const TwoFactor = async () => {
+    const session = await auth()
     return (
         <SettingCard>
             <div className='flex justify-between items-center'>
                 <p className='text-sm font-medium'>Two Factor Authentication</p>
                 {
-                    false ? <Button size={"sm"} className='bg-blue-600 text-white hover:bg-blue-700'><Check className='w-4 h-4' /> On</Button>
+                    session?.user.isTwoFactorEnabled ? <Button size={"sm"} className='bg-blue-600 text-white hover:bg-blue-700'><Check className='w-4 h-4' /> On</Button>
                         : <Button size={"sm"} className='bg-red-600 text-white hover:bg-red-700'><X className='w-4 h-4' /> Off</Button>
                 }
             </div>
--- server/auth.ts
+++ server/auth.ts
@@ -6,13 +6,41 @@ import GitHub from "next-auth/providers/github"
 import Credentials from "next-auth/providers/credentials"
 import { loginSchema } from "@/types/login-schema"
 import { eq } from "drizzle-orm"
-import { users } from "./schema"
+import { accounts, users } from "./schema"
 import bcrypt from "bcrypt"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: DrizzleAdapter(db),
     secret: process.env.AUTH_SECRET,
     session: { strategy: "jwt" },
+    callbacks: {
+        async session({ session, token }) {
+            if (session && token.sub) { session.user.id = token.sub }
+            if (session.user && token.role) { session.user.role = token.role as string }
+            if (session) {
+                session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
+                session.user.name = token.name as string
+                session.user.email = token.email as string
+                session.user.image = token.image as string
+                session.user.isOauth = token.isOauth as boolean
+            }
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token// sub is the user id
+            const existingUser = await db.query.users.findFirst({ where: eq(users.id, token.sub) })
+            if (!existingUser) return token
+
+            const existingAccount = await db.query.accounts.findFirst({ where: eq(accounts.userId, existingUser.id) })
+            token.isOauth = existingAccount ? true : false // !!existingAccount
+            token.name = existingUser.name
+            token.email = existingUser.email
+            token.image = existingUser.image
+            token.role = existingUser.role
+            token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
+            return token
+        }
+    },
     providers: [
         GitHub({
             clientId: process.env.AUTH_GITHUB_ID,
